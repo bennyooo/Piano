da8280212192ce969c185f616dd34e787ae17ad6
bennyooo <benjamin2@web.de>
2016-04-30 12:08:03 +0200

    Main angepasst

--- Piano/src/application/Main.java
+++ Piano/src/application/Main.java
@@ -1,25 +1,30 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	public void start(Stage primaryStage) throws IOException{
+
+			Parent root = FXMLLoader.load(getClass().getResource("KeyLayout.fxml"));
+			Scene scene = new Scene(root);
+
 			primaryStage.setScene(scene);
+			primaryStage.sizeToScene();
+
+			primaryStage.setTitle("Piano Test");
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
