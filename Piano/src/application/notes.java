0db0372ce3772adea1710010dd3922b88542bda1
bennyooo <benjamin2@web.de>
2016-05-13 19:14:41 +0200

    test

--- Piano/src/application/notes.java
+++ Piano/src/application/notes.java
@@ -3,6 +3,8 @@ package application;
 public enum notes {
 	KEYC("C:\\Users\\Benjamin\\Downloads\\notes\\c1.mid");
 
+	private String note;
 	private notes(String location) {
+		this.note=location;
 	}
 }
