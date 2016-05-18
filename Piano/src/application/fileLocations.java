5d10f41b6b12e14af5f9714278aa033ec9eeb721
Benjamin Meyer <benjamin2@web.de>
2016-05-18 21:20:21 +0200

    fileLocations Klasse erstellt

---
+++ Piano/src/application/fileLocations.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.*;
+
+
+public class fileLocations{
+
+	public static String getLocation(int i){
+		String location = "";
+		ArrayList<String> fileLocations = new ArrayList<String>();
+		Collections.addAll(fileLocations, 	"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\c1.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\d.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\e.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\f.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\g.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\a.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\b.mid",
+											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\c2.mid");
+		location = fileLocations.get(i);
+		return location;
+	}
+}
