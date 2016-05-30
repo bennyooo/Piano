3994af4e9e3c00828c66b68a2cf9c8c01526690d
Benjamin Meyer <benjamin2@web.de>
2016-05-30 18:46:07 +0200

    Klasse mySequencer mit statischem Sequencer erstellt

--- Piano/src/application/KeyController.java
+++ Piano/src/application/KeyController.java
@@ -1,6 +1,5 @@
 package application;
 
-import javafx.*;
 import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -16,77 +15,51 @@ import javax.sound.midi.*;
 
 public class KeyController {
 
-	public void playSequence(String location){
-
-		String jLocation = location.replace("\\", "/");
-
-		try{
-			Sequencer sq = MidiSystem.getSequencer();
-			InputStream midiFile = new BufferedInputStream(new FileInputStream(jLocation));
-
-			sq.open();
-			sq.setSequence(midiFile);
-			sq.start();
-			if (!(sq.isRunning())){
-				sq.stop();
-				sq.close();
-			}
-		}
-		catch(MidiUnavailableException e){
-			e.printStackTrace();
-		}
-		catch (InvalidMidiDataException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	@FXML
 	private Text tFeld;
 
 	@FXML
-	private void playNote(ActionEvent event){
+	/**
+	 * Spielt Ton der Taste
+	 * @param event
+	 */
+	private void playNote(ActionEvent event) throws MidiUnavailableException{
 
 		Button x = (Button) event.getSource();
+		mySequencer ms = new mySequencer();
 
 		if(x.getId().equals("keyC")){
-			playSequence(fileLocations.getLocation(0));
-			tFeld.setText("Taste C gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(0));
 		}
 		else if(x.getId().equals("keyD")){
-			playSequence(fileLocations.getLocation(1));
-			tFeld.setText("Taste D gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(1));
 		}
 		else if(x.getId().equals("keyE")){
-			playSequence(fileLocations.getLocation(2));
-			tFeld.setText("Taste E gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(2));
 		}
 		else if(x.getId().equals("keyF")){
-			playSequence(fileLocations.getLocation(3));
-			tFeld.setText("Taste F gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(3));
 		}
 		else if(x.getId().equals("keyG")){
-			playSequence(fileLocations.getLocation(4));
-			tFeld.setText("Taste G gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(4));
 		}
 		else if(x.getId().equals("keyA")){
-			playSequence(fileLocations.getLocation(5));
-			tFeld.setText("Taste A gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(5));
 		}
 		else if(x.getId().equals("keyH")){
-			playSequence(fileLocations.getLocation(6));
-			tFeld.setText("Taste H gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(6));
 		}
 		else if(x.getId().equals("keyC2")){
-			playSequence(fileLocations.getLocation(7));
-			tFeld.setText("Taste C2 gedr�ckt!");
+			ms.setMySequence(fileLocations.getLocation(7));
 		}
 		else tFeld.setText("Unbekannte Taste gedr�ckt");
 	}
 	@FXML
+	/**
+	 * Beenden des Tons
+	 * @param event
+	 */
 	private void stopNote(ActionEvent event){
-		
+
 	}
 }
