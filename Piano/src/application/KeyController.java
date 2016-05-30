5847f3d285e0302446a084ed624708d6c198e58d
Benjamin Meyer <benjamin2@web.de>
2016-05-30 20:07:42 +0200

    Halbnoten hinzugefügt

--- Piano/src/application/KeyController.java
+++ Piano/src/application/KeyController.java
@@ -26,28 +26,42 @@ public class KeyController {
 		if(x.getId().equals("keyC")){
 			ms.setMySequence(fileLocations.getLocation(0));
 		}
-		else if(x.getId().equals("keyD")){
+		else if(x.getId().equals("keyDFlat")){
 			ms.setMySequence(fileLocations.getLocation(1));
 		}
-		else if(x.getId().equals("keyE")){
+		else if(x.getId().equals("keyD")){
 			ms.setMySequence(fileLocations.getLocation(2));
 		}
-		else if(x.getId().equals("keyF")){
-			ms.setMySequence(fileLocations.getLocation(3));
-		}
-		else if(x.getId().equals("keyG")){
+        else if(x.getId().equals("keyEFlat")){
+            ms.setMySequence(fileLocations.getLocation(3));
+        }
+		else if(x.getId().equals("keyE")){
 			ms.setMySequence(fileLocations.getLocation(4));
 		}
-		else if(x.getId().equals("keyA")){
+		else if(x.getId().equals("keyF")){
 			ms.setMySequence(fileLocations.getLocation(5));
 		}
-		else if(x.getId().equals("keyH")){
-			ms.setMySequence(fileLocations.getLocation(6));
-		}
-		else if(x.getId().equals("keyC2")){
+        else if(x.getId().equals("keyGFlat")){
+            ms.setMySequence(fileLocations.getLocation(6));
+        }
+		else if(x.getId().equals("keyG")){
 			ms.setMySequence(fileLocations.getLocation(7));
 		}
-		else tFeld.setText("Unbekannte Taste gedrueckt");
+        else if(x.getId().equals("keyAFlat")){
+            ms.setMySequence(fileLocations.getLocation(8));
+        }
+		else if(x.getId().equals("keyA")){
+			ms.setMySequence(fileLocations.getLocation(9));
+		}
+        else if(x.getId().equals("keyHFlat")){
+            ms.setMySequence(fileLocations.getLocation(10));
+        }
+		else if(x.getId().equals("keyH")){
+			ms.setMySequence(fileLocations.getLocation(11));
+		}
+		else if(x.getId().equals("keyC2")) {
+            ms.setMySequence(fileLocations.getLocation(12));
+        }
 	}
 	@FXML
 	/**
--- Piano/src/application/fileLocations.java
+++ Piano/src/application/fileLocations.java
@@ -11,10 +11,14 @@ class fileLocations{
 		Collections.addAll(fileLocations, 	"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\c1.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\db.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\d.mid",
+				                            "C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\eb.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\e.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\f.mid",
+                                            "C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\fs.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\g.mid",
+                                            "C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\gs.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\a.mid",
+                                            "C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\as.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\b.mid",
 											"C:\\Users\\Benjamin\\Documents\\GitHub\\Piano\\Piano\\notes\\c2.mid");
 		location = fileLocations.get(i);
